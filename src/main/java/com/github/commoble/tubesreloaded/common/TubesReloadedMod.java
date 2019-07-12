ba869ac22ccfcc1cca3e53b53b5c23ad5f24503c
Commoble <josephbettendorff@yahoo.com>
2019-07-12 15:33:46 -0500

    Updated version number for next release

--- src/main/java/com/github/commoble/tubesreloaded/common/TubesReloadedMod.java
+++ src/main/java/com/github/commoble/tubesreloaded/common/TubesReloadedMod.java
@@ -7,8 +7,6 @@ import net.minecraftforge.fml.common.Mod;
 public class TubesReloadedMod
 {
 	public static final String MODID = "tubesreloaded";
-    public static final String VERSION = "1.0.0.0";
-    public static final String NAME="Tubes Reloaded";
 	
 	public TubesReloadedMod()
 	{
