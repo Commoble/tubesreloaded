eac8701540622436f89d8f0ec300794b382cc346
Commoble <josephbettendorff@yahoo.com>
2019-11-22 20:41:13 -0600

    Osmosis filters render properly for each facing

--- src/main/java/com/github/commoble/tubesreloaded/client/OsmosisFilterTileEntityRenderer.java
+++ src/main/java/com/github/commoble/tubesreloaded/client/OsmosisFilterTileEntityRenderer.java
@@ -5,6 +5,7 @@ import java.util.Random;
 import com.github.commoble.tubesreloaded.common.ConfigValues;
 import com.github.commoble.tubesreloaded.common.blocks.filter.FilterTileEntity;
 import com.github.commoble.tubesreloaded.common.blocks.filter.OsmosisFilterBlock;
+import com.github.commoble.tubesreloaded.common.blocks.filter.OsmosisSlimeBlock;
 import com.github.commoble.tubesreloaded.common.registry.BlockRegistrar;
 import com.mojang.blaze3d.platform.GlStateManager;
 
@@ -17,6 +18,7 @@ import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.texture.AtlasTexture;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
+import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
@@ -34,100 +36,119 @@ public class OsmosisFilterTileEntityRenderer extends FilterTileEntityRenderer
 	private void renderSlime(FilterTileEntity te, double x, double y, double z, float partialTicks)
 	{
 		BlockPos blockpos = te.getPos();
-		BlockState renderState = BlockRegistrar.OSMOSIS_SLIME.getDefaultState();
+		BlockState filterState = te.getBlockState();
+		Direction dir = filterState.get(OsmosisFilterBlock.FACING);
+		BlockState renderState = BlockRegistrar.OSMOSIS_SLIME.getDefaultState().with(OsmosisSlimeBlock.FACING, dir);
+		long transferhash = blockpos.hashCode();
+		int rate = ConfigValues.osmosis_filter_transfer_rate;
+		double ticks = te.getWorld().getDayTime() + transferhash + partialTicks;
+		double minScale = 0.2D;
+		double lengthScale = minScale + (te.getBlockState().get(OsmosisFilterBlock.TRANSFERRING_ITEMS)
+			? (-Math.cos(2 * Math.PI * ticks / rate) + 1D) * 0.25D
+			: (-Math.cos(2 * Math.PI * ticks / rate) + 1D) * 0.25D);//0D);
+		double lengthTranslateFactor = 1D - lengthScale;
+		
+		
+		
+		
+		double translateX, translateY, translateZ, scaleX, scaleY, scaleZ;
+
+		double zFightFix = 0.9999D;
+		scaleZ = zFightFix;
+		
+		int dirOffsetX = dir.getXOffset();
+		int dirOffsetY = dir.getYOffset();
+		int dirOffsetZ = dir.getZOffset();
+		
+		scaleX = dirOffsetX == 0 ? zFightFix : lengthScale;
+		scaleY = dirOffsetY == 0 ? zFightFix : lengthScale;
+		scaleZ = dirOffsetZ == 0 ? zFightFix : lengthScale;
+		
+		int translateFactorX = dirOffsetX == 0 ? 0 : 1;
+		int translateFactorY = dirOffsetY == 0 ? 0 : 1;
+		int translateFactorZ = dirOffsetZ == 0 ? 0 : 1;
+		
+		double tX = dirOffsetX > 0 ? x+1D : x;
+		double tY = dirOffsetY > 0 ? y+1D : y;
+		double tZ = dirOffsetZ > 0 ? z+1D : z;
+		
+		translateX = translateFactorX * (tX * lengthTranslateFactor - 0.125D*dirOffsetX);
+		translateY = translateFactorY * (tY * lengthTranslateFactor - 0.125D*dirOffsetY);
+		translateZ = translateFactorZ * (tZ * lengthTranslateFactor - 0.125D*dirOffsetZ);
+		
+//		if (dir.getZOffset() < 0)
+//		{
+//			translateX = -dir.getXOffset() * x * lengthTranslateFactor;
+//			translateY = -dir.getYOffset() * y * lengthTranslateFactor;
+//			translateZ = -dir.getZOffset() * (z * lengthTranslateFactor + 0.125D);
+//		}
+//		else if (dir.getZOffset() > 0)
+//		{
+//			translateX = dir.getXOffset() * x * lengthTranslateFactor;
+//			translateY = dir.getYOffset() * y * lengthTranslateFactor;
+//			translateZ = dir.getZOffset() * ((z+1D)*lengthTranslateFactor - 0.125D);
+//		}
+//
+//		else if (dir.getXOffset() < 0)
+//		{
+//			translateX = -dir.getXOffset() * (x * lengthTranslateFactor + 0.125D);
+//			translateY = -dir.getYOffset() * y * lengthTranslateFactor;
+//			translateZ = -dir.getZOffset() * z * lengthTranslateFactor;
+//		}
+//		else if (dir.getXOffset() > 0)
+//		{
+//			translateX = dir.getXOffset() * ((x+1D) * lengthTranslateFactor - 0.125D);
+//			translateY = dir.getYOffset() * y * lengthTranslateFactor;
+//			translateZ = dir.getZOffset() * z * lengthTranslateFactor;
+//		}
+//
+//		else if (dir.getYOffset() < 0)
+//		{
+//			translateX = -dir.getXOffset() * (x * lengthTranslateFactor + 0.125D);
+//			translateY = -dir.getYOffset() * (y * lengthTranslateFactor + 0.125D);
+//			translateZ = -dir.getZOffset() * z * lengthTranslateFactor;
+//		}
+//		else // y > 0
+//		{
+//			translateX = dir.getXOffset() * ((x+1D) * lengthTranslateFactor - 0.125D);
+//			translateY = dir.getYOffset() * ((y+1D) * lengthTranslateFactor - 0.125D);
+//			translateZ = dir.getZOffset() * z * lengthTranslateFactor;
+//		}
+		
+		GlStateManager.pushMatrix();
+		//GlStateManager.translated(0, 0, z * lengthTranslateFactor + 0.125D);
+		GlStateManager.translated(translateX, translateY, translateZ);
+		GlStateManager.scaled(scaleX, scaleY, scaleZ);
+
+		Tessellator tessellator = Tessellator.getInstance();
+		BufferBuilder bufferbuilder = tessellator.getBuffer();
+		this.bindTexture(AtlasTexture.LOCATION_BLOCKS_TEXTURE);
+		RenderHelper.disableStandardItemLighting();
+		GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
+		GlStateManager.enableBlend();
+		GlStateManager.disableCull();
+		if (Minecraft.isAmbientOcclusionEnabled())
 		{
-			long transferhash = blockpos.hashCode();
-			int rate = ConfigValues.osmosis_filter_transfer_rate;
-			double ticks = te.getWorld().getDayTime() + transferhash + partialTicks;
-			double minZScale = 0.2D;
-			double zScale = minZScale + (te.getBlockState().get(OsmosisFilterBlock.TRANSFERRING_ITEMS)
-				? (-Math.cos(2*Math.PI * ticks / rate) + 1D) * 0.25D
-				: 0D);
-			double zTranslateFactor = 1D - zScale;
-//			double zScaleFactor = zScale*zScale/8D - 7D*zScale/8D + 3D/4D;
-//			double zOffset = (zScale * zScale - 7D*zScale + 6D)/64D;
-			GlStateManager.pushMatrix();
-			GlStateManager.translated(0, 0, z*zTranslateFactor + 0.125D);
-			GlStateManager.scaled(0.999D,0.999D,zScale);
-			
-			Tessellator tessellator = Tessellator.getInstance();
-			BufferBuilder bufferbuilder = tessellator.getBuffer();
-			this.bindTexture(AtlasTexture.LOCATION_BLOCKS_TEXTURE);
-			RenderHelper.disableStandardItemLighting();
-			GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
-			GlStateManager.enableBlend();
-			GlStateManager.disableCull();
-			if (Minecraft.isAmbientOcclusionEnabled())
-			{
-				GlStateManager.shadeModel(7425);
-			} else
-			{
-				GlStateManager.shadeModel(7424);
-			}
-
-			BlockModelRenderer.enableCache();
-			bufferbuilder.begin(7, DefaultVertexFormats.BLOCK);
-			bufferbuilder.setTranslation(x - blockpos.getX() + 0.000D, y - blockpos.getY()+ 0.000D, z - blockpos.getZ() + 0.000D);
-			World world = this.getWorld();
-			{
-				this.renderStateModel(blockpos, renderState, bufferbuilder, world, false);
-			}
-
-			bufferbuilder.setTranslation(0.0D, 0.0D, 0.0D);
-			tessellator.draw();
-			BlockModelRenderer.disableCache();
-			RenderHelper.enableStandardItemLighting();
-			
-			GlStateManager.popMatrix();
+			GlStateManager.shadeModel(7425);
+		} else
+		{
+			GlStateManager.shadeModel(7424);
 		}
-		// // the client setup event runs before the model registry event
-		// // so we can't cache this when the renderer is instantiated
-		// // (unless we bind the renderer in a different event?)
-		//// final IBakedModel BAKED_SLIME_MODEL = ClientEventHandler.BAKED_SLIME_MODEL;
-		//
-		// int i = 1;
-		//// GlStateManager.texParameter(3553, 10242, 10497);
-		//// GlStateManager.texParameter(3553, 10243, 10497);
-		//// GlStateManager.disableLighting();
-		//// GlStateManager.disableCull();
-		//// GlStateManager.disableBlend();
-		//// GlStateManager.depthMask(true);
-		// this.bindTexture(AtlasTexture.LOCATION_BLOCKS_TEXTURE);
-		//
-		//
-		// GlStateManager.pushMatrix();
-		// GlStateManager.disableLighting();
-		// GlStateManager.enableBlend();
-		// GlStateManager.blendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA,
-		// GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA,
-		// GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
-		//
-		// Tessellator tessellator = Tessellator.getInstance();
-		// BufferBuilder bufferbuilder = tessellator.getBuffer();
-		//
-		// bufferbuilder.begin(7, DefaultVertexFormats.BLOCK);
-		//
-		// GlStateManager.translated(x + 0.5D, y+1D, z + 0.5D);
-		// BlockRendererDispatcher blockrendererdispatcher =
-		// Minecraft.getInstance().getBlockRendererDispatcher();
-		// World world = te.getWorld();
-		// BlockState state = te.getBlockState();
-		// DirectionProperty FACING = BlockStateProperties.FACING;
-		// blockrendererdispatcher.getBlockModelRenderer().renderModel(world,
-		// blockrendererdispatcher.getModelForState(BlockRegistrar.OSMOSIS_SLIME.getDefaultState().with(FACING,
-		// state.get(FACING))),
-		// state,
-		// te.getPos(),
-		// bufferbuilder,
-		// false, // "checkSides"
-		// world.rand,
-		// world.getSeed(),
-		// EmptyModelData.INSTANCE);
-		// tessellator.draw();
-		//
-		// GlStateManager.enableLighting();
-		// GlStateManager.disableBlend();
-		// GlStateManager.popMatrix();
+
+		BlockModelRenderer.enableCache();
+		bufferbuilder.begin(7, DefaultVertexFormats.BLOCK);
+		bufferbuilder.setTranslation(x - blockpos.getX(), y - blockpos.getY(), z - blockpos.getZ());
+		World world = this.getWorld();
+		{
+			this.renderStateModel(blockpos, renderState, bufferbuilder, world, false);
+		}
+
+		bufferbuilder.setTranslation(0.0D, 0.0D, 0.0D);
+		tessellator.draw();
+		BlockModelRenderer.disableCache();
+		RenderHelper.enableStandardItemLighting();
+
+		GlStateManager.popMatrix();
 	}
 
 	private boolean renderStateModel(BlockPos pos, BlockState state, BufferBuilder buffer, World p_188186_4_, boolean checkSides)
