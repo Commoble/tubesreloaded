bfff2ce98c00465bb6a434f9e28df41d72527e1a
Commoble <josephbettendorff@yahoo.com>
2020-06-25 16:07:42 -0500

    Add tech from More Red to set groundwork for remote tube connections

--- src/main/java/com/github/commoble/tubesreloaded/registry/Names.java
+++ src/main/java/com/github/commoble/tubesreloaded/registry/Names.java
@@ -5,8 +5,9 @@ import java.util.Arrays;
 import net.minecraft.item.DyeColor;
 
 // would put them in BlockRegistrar but the global objectholder doesn't jib with it
-public class BlockNames
+public class Names
 {
+	// blocks and blockitems
 	public static final String TUBE = "tube";
 	public static final String SHUNT = "shunt";
 	public static final String LOADER = "loader";	
@@ -16,6 +17,12 @@ public class BlockNames
 	public static final String OSMOSIS_FILTER = "osmosis_filter";
 	public static final String OSMOSIS_SLIME = "osmosis_slime";
 	public static final String DISTRIBUTOR = "distributor";
+	
+	// other items
+	public static final String TUBING_PLIERS = "tubing_pliers";
+	
+	// misc
+	public static final String TUBES_IN_CHUNK = "tubes_in_chunk";
 	
 	public static final String[] COLORED_TUBE_NAMES = Arrays.stream(DyeColor.values()).map(color -> color.toString() + "_tube").toArray(String[]::new);
 }
